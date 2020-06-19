a12fe8b33ed78b6d71f105ef7e3960663411f3f4
Martin <martintonts@gmail.com>
2020-06-19 10:03:58 +0300

    changed GA site script

--- app/server.js
+++ app/server.js
@@ -286,13 +286,14 @@ export default function(req, res, next) {
       if (config.GTMid) {
         // Google Tag Manager script
         res.write(
-          `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${
-              config.GTMid
-            }');</script>\n`,
+          `<!-- Global site tag (gtag.js) - Google Analytics -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169954573-1"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-169954573-1');
+          </script>\n`,
         );
       }
 
@@ -361,12 +362,12 @@ export default function(req, res, next) {
         const content =
           relayData != null
             ? getContent(
-                context,
-                relayData.props,
-                locale,
-                req.headers['user-agent'],
-                req,
-              )
+            context,
+            relayData.props,
+            locale,
+            req.headers['user-agent'],
+            req,
+            )
             : undefined;
 
         const head = Helmet.rewind();
